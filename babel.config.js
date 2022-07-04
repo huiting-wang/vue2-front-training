ac8e8b0bc25851fe743248f6c3620cc9b476cbdc
Grace Wang <grace.wang@staff.nueip.com>
2022-07-04 15:16:41 +0800

    Modify vscode and babel settings option / Install VueUse

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    ["@vue/cli-plugin-babel/preset", { useBuiltIns: "usage", corejs: 3 }],
+  ],
+  plugins: [
+    [
+      "component",
+      {
+        ext: ".scss",
+        libraryName: "element-ui",
+        styleLibrary: {
+          name: "~node_modules/element-ui/packages/theme-chalk/src",
+          base: true,
+          path: "[module].scss",
+          mixin: true,
+        },
+      },
+    ],
+  ],
+  env: {
+    test: {
+      plugins: ["transform-require-context"],
+    },
+  },
 };
