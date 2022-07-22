47b1f64759ad627e73a42f3594f702e64d874df9
Grace Wang <grace.wang@staff.nueip.com>
2022-07-22 12:51:38 +0800

    Update Package

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    ['@vue/cli-plugin-babel/preset', { useBuiltIns: 'usage', corejs: '3.23' }],
+    [
+      '@vue/cli-plugin-babel/preset',
+      { useBuiltIns: 'usage', corejs: '3.23', modules: false },
+    ],
   ],
   plugins: [
     'lodash',
